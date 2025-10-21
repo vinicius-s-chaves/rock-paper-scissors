bcafb18caeed25512c774cb5e90d61aca717919f
Vinicius Chaves <vinicius@Xubuntu.myguest.virtualbox.org>
2025-10-20 21:13:01 -0300

    Add getHumanChoice function. This gets the player move

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function getHumanChoice(humanMove) {
+    let moveChoice = prompt('What\'s your move? \nRock (r), Paper (p) or Scissors (s)?')
+    return moveChoice
+}
+
+
 let getComputerChoice = function(computerMove) {
     let randomChoice = (Math.random().toFixed(3)) * 100
 
